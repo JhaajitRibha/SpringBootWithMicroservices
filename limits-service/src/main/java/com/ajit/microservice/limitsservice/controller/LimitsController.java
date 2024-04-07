c13901c9b425663ee717b449c7d3231f000cffa0
JhaajitRibha <jhaajit1996@gmail.com>
2024-04-07 12:50:54 +0530

    added configurations files

--- limits-service/src/main/java/com/ajit/microservice/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/ajit/microservice/limitsservice/controller/LimitsController.java
@@ -1,16 +1,21 @@
 package com.ajit.microservice.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ajit.microservice.limitsservice.beans.Limits;
+import com.ajit.microservice.limitsservice.configurations.LimitConfigurations;
 
 @RestController
 public class LimitsController {
 	
+	@Autowired
+	private LimitConfigurations limitConfigurations;
+	
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1,1000);
+		return new Limits(limitConfigurations.getMinimum(),limitConfigurations.getMaximum());
 	}
 
 }
