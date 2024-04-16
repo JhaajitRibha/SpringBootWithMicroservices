6ca8fb45ee95d42773cca06d169612aa2d09cf07
JhaajitRibha <jhaajit1996@gmail.com>
2024-04-16 19:22:40 +0530

    updated

--- currency-exchange-server/src/main/java/com/ajit/microservices/currencyexchangeserver/controller/CircuitBreakerController.java
+++ currency-exchange-server/src/main/java/com/ajit/microservices/currencyexchangeserver/controller/CircuitBreakerController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -25,6 +26,7 @@ public class CircuitBreakerController {
 //	@Retry(name="sample-api",fallbackMethod="hardCodedResponse")
 //	@CircuitBreaker(name="default",fallbackMethod="hardCodedResponse")
 	@RateLimiter(name="default")
+	@Bulkhead(name="default")
 	public String sampliApi() {
 		logger.info("received sample api call ....");
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummyapi", String.class);
