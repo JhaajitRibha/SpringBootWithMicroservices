b83261ea527bfbfa254d94462ed6a74f09aea18a
JhaajitRibha <jhaajit1996@gmail.com>
2024-04-16 19:19:05 +0530

    added bulkhead,ratelimiter config

--- currency-exchange-server/src/main/java/com/ajit/microservices/currencyexchangeserver/controller/CircuitBreakerController.java
+++ currency-exchange-server/src/main/java/com/ajit/microservices/currencyexchangeserver/controller/CircuitBreakerController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 
@@ -20,7 +22,9 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 	
 	@GetMapping("/sample-api")
-	@Retry(name="sample-api",fallbackMethod="hardCodedResponse")
+//	@Retry(name="sample-api",fallbackMethod="hardCodedResponse")
+//	@CircuitBreaker(name="default",fallbackMethod="hardCodedResponse")
+	@RateLimiter(name="default")
 	public String sampliApi() {
 		logger.info("received sample api call ....");
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummyapi", String.class);
